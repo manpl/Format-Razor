5569f9676e2842f493d215316c7ec702c9876ade
Marcinn <nawrockim@op.pl>
2015-06-23 14:22:22 +0200

    calling razor

--- FormatRazor.cs
+++ FormatRazor.cs
@@ -1,41 +1,71 @@
-﻿using System.Management.Automation;
+﻿using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Management.Automation;
 
 namespace Format_Razor
 {
-    [Cmdlet(VerbsCommon.Format, "Razor")]
+    [Cmdlet(VerbsCommon.Format, "Razor", DefaultParameterSetName="Inline")]
+    [Description("Creates the document using specified template and model")]
     public class FormatRazor : PSCmdlet
     {
         [Parameter(Mandatory = true,
             Position = 0,
             ValueFromPipeline = true,
-            ParameterSetName = "Inline",
+            ParameterSetName = ParamSets.Inline,
             HelpMessage = "Model object to be used within template")]
-        [Parameter(ParameterSetName = "External")]
+        [Parameter(ParameterSetName = ParamSets.External)]
         [ValidateNotNullOrEmpty]
         public object Model { get; set; }
 
-        [Parameter(ParameterSetName="External",
+        [Parameter(ParameterSetName = ParamSets.External,
+            Mandatory=true,
             HelpMessage="Path to external template file")]
         [ValidateNotNullOrEmpty]
         public string TemplatePath { get; set; }
 
-        [Parameter(ParameterSetName="Inline",
+        [Parameter(ParameterSetName = ParamSets.Inline,
+            Mandatory=true,
             HelpMessage = "Inline template")]
         [ValidateNotNullOrEmpty]
         public string Template { get; set; }
 
-        private bool IsDebug
+        protected override void BeginProcessing()
         {
-            get
+            var config = new TemplateServiceConfiguration();
+            //config.Language = Language.VisualBasic; // VB.NET as template language.
+            //config.EncodedStringFactory = new RawStringFactory(); // Raw string encoding.
+            //config.EncodedStringFactory = new HtmlEncodedStringFactory(); // Html encoding.
+            //config.Debug = IsDebug;
+            config.CachingProvider = new DefaultCachingProvider(t => { });
+            config.DisableTempFileLocking = true;
+            Engine.Razor = RazorEngineService.Create(config); ;
+
+            if (ParameterSetName == ParamSets.External)
             {
-                return (bool)this.GetVariableValue("PSDebugContext");
+                ValidateExternal();
+            }
+
+            base.BeginProcessing();
+        }
+
+        private void ValidateExternal()
+        {
+            if (!File.Exists(this.TemplatePath))
+            {
+                var ex = new FileNotFoundException("template file cannot be found");
+                var error = new ErrorRecord(ex, "PathNotFound", ErrorCategory.InvalidArgument, null);
+                this.ThrowTerminatingError(error);
             }
         }
 
         protected override void ProcessRecord()
         {
-            WriteDebug("ProcessRecord");
-            WriteObject("---" + Model.ToString());
+            WriteDebug("ProcessRecord, paramSet:" + this.ParameterSetName);
+            WriteObject(Engine.Razor.RunCompile(Template, "templateKey", null, Model));
             base.ProcessRecord();
         }
     }
---
+++ ParamSets.cs
@@ -0,0 +1,8 @@
+﻿namespace Format_Razor
+{
+    public class ParamSets
+    {
+        public const string Inline = "Inline";
+        public const string External = "External";
+    }
+}

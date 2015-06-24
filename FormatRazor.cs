58f30d796f628d922f969ba34278e3a52e093f33
Marcinn <nawrockim@op.pl>
2015-06-24 14:21:07 +0200

    removed ParamSet from Model parameter. added FileTemplate for templates from disk

--- FormatRazor.cs
+++ FormatRazor.cs
@@ -1,4 +1,5 @@
-﻿using RazorEngine;
+﻿using Format_Razor.RazorEngine;
+using RazorEngine;
 using RazorEngine.Configuration;
 using RazorEngine.Templating;
 using System;
@@ -8,37 +9,32 @@ using System.Management.Automation;
 
 namespace Format_Razor
 {
-    [Cmdlet(VerbsCommon.Format, "Razor", DefaultParameterSetName="Inline")]
+    [Cmdlet(VerbsCommon.Format, "Razor", DefaultParameterSetName = "Inline")]
     [Description("Creates the document using specified template and model")]
     public class FormatRazor : PSCmdlet
     {
         [Parameter(Mandatory = true,
             Position = 0,
             ValueFromPipeline = true,
-            ParameterSetName = ParamSets.Inline,
-            HelpMessage = "Model object to be used within template")]
-        [Parameter(Mandatory = true,
-            Position = 0,
-            ValueFromPipeline = true,
-            ParameterSetName = ParamSets.External,
             HelpMessage = "Model object to be used within template")]
         [ValidateNotNullOrEmpty]
         public object Model { get; set; }
 
         [Parameter(ParameterSetName = ParamSets.External,
-            Mandatory=true,
-            HelpMessage="Path to external template file")]
+            Mandatory = true,
+            HelpMessage = "Path to external template file")]
         [ValidateNotNullOrEmpty]
         public string TemplatePath { get; set; }
 
         [Parameter(ParameterSetName = ParamSets.Inline,
-            Mandatory=true,
+            Mandatory = true,
             HelpMessage = "Inline template")]
         [ValidateNotNullOrEmpty]
         public string Template { get; set; }
 
         protected override void BeginProcessing()
         {
+            WriteDebug("BeginProcessing paramSet:" + this.ParameterSetName);
             var config = new TemplateServiceConfiguration();
             //config.Language = Language.VisualBasic; // VB.NET as template language.
             //config.EncodedStringFactory = new RawStringFactory(); // Raw string encoding.
@@ -47,6 +43,7 @@ namespace Format_Razor
             config.CachingProvider = new DefaultCachingProvider(t => { });
             config.DisableTempFileLocking = true;
             Engine.Razor = RazorEngineService.Create(config); ;
+
 
             if (ParameterSetName == ParamSets.External)
             {
@@ -66,10 +63,24 @@ namespace Format_Razor
             }
         }
 
+        public ITemplateSource GetTemplateSource()
+        {
+            if (this.ParameterSetName == ParamSets.Inline)
+            {
+                return new LoadedTemplateSource(Template);
+            }
+            else
+            {
+                return new FileTemplate(this.TemplatePath);
+            }
+        }
+
         protected override void ProcessRecord()
         {
             WriteDebug("ProcessRecord, paramSet:" + this.ParameterSetName);
-            WriteObject(Engine.Razor.RunCompile(Template, "templateKey", null, Model));
+
+            var template = GetTemplateSource();
+            WriteObject(Engine.Razor.RunCompile(template, "templateKey", null, Model));
             base.ProcessRecord();
         }
     }
---
+++ RazorEngine/FileTemplate.cs
@@ -0,0 +1,36 @@
+﻿using RazorEngine.Templating;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Format_Razor.RazorEngine
+{
+    public class FileTemplate : ITemplateSource
+    {
+        public FileTemplate(string path)
+        {
+            this.TemplateFile = path;
+            this.Template = File.ReadAllText(path);
+        }
+
+        public System.IO.TextReader GetTemplateReader()
+        {
+            return new StringReader(this.Template);
+        }
+
+        public string Template 
+        { 
+            get;
+            private set;
+        }
+
+        public string TemplateFile
+        {
+            get;
+            private set;
+        }
+    }
+}

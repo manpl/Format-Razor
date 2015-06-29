64a435e0ca058943f33517321ad76da9376fd310
Marcinn <nawrockim@op.pl>
2015-06-29 20:24:20 +0200

    Formatting tabs to spaces :)

--- Format-Razor-Tests/tests.ps1
+++ Format-Razor-Tests/tests.ps1
@@ -7,42 +7,48 @@ Import-Module $modulePath
 
 Describe "Format-Razor parameters validation" {
 
-	$pwd = (pwd)
-	
-	It "Model object cannot be null" {
+    $pwd = (pwd)
+    
+    It "Model object cannot be null" {
         { Format-Razor -Model $null -Template "aa" } | Should Throw
     }
 
-	It "Template cannot be null" {
-		$pwd = (pwd)
+    It "Template cannot be null" {
+        $pwd = (pwd)
         { Format-Razor -Model $pwd -Template $null } | Should Throw
     }
 
-	It "Template cannot be empty" {
+    It "Template cannot be empty" {
         { Format-Razor -Model $pwd -Template "" } | Should Throw
     }
 
-	It "TemplatePath cannot be empty" {
+    It "TemplatePath cannot be empty" {
         { Format-Razor -Model $pwd -TemplatePath "" } | Should Throw
     }
 
-	It "TemplatePath cannot be null" {
+    It "TemplatePath cannot be null" {
         { Format-Razor -Model $pwd -TemplatePath $null } | Should Throw
     }
 
-	It "TemplatePath and Template are exclusive paramsets" {
+    It "TemplatePath and Template are exclusive paramsets" {
         { Format-Razor -Model $pwd -TemplatePath "templatePath" -Template "Template"} | Should Throw
     }
 
-	It "TemplatePath must exist" {
+    It "TemplatePath must exist" {
         { Format-Razor -Model $pwd -TemplatePath "templatePath" } | Should Throw
     }
 
-	It "Array Model passed, array result returned" {
-		$model = (1,2,3,4,5)
-		$result = ($model | Format-Razor -Template "@Model-@Model")
-		
-		$result  | Should BeExactly ("1-1", "2-2", "3-3", "4-4", "5-5")
-		$result.Length | Should BeExactly 5
-	}
+    It "Array Model passed, array result returned" {
+        $model = (1,2,3,4,5)
+        $result = ($model | Format-Razor -Template "@Model-@Model")
+        
+        $result  | Should BeExactly ("1-1", "2-2", "3-3", "4-4", "5-5")
+        $result.Length | Should BeExactly 5
+    }
+
+    It "String model passed, string result returned"{
+        $model = "Element"
+        $result = ($model | Format-Razor -Template "@Model.Length")
+        $result | Should BeExactly 7
+    }
 }
